0ce5606cbbd2eff2145cd5349ac40835ec9b529e
Sergio <sacortesh@github.com>
2015-08-16 22:02:26 +0200

    Uploading work done through the weekend. Keep the hype up Serge, we'll do this

---
+++ weekend_attack/app.js
@@ -0,0 +1,93 @@
+var app = angular.module('weekendAttack', ['ui.router']);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+		$stateProvider
+		.state('home',{
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		});
+
+		$stateProvider.state('objects', {
+			url: '/objects/{id}',
+			templateUrl: '/objects.html',
+			controller: 'ObjectsCtrl'
+		});
+
+	$urlRouterProvider.otherwise('home');
+	}]);
+
+app.controller('MainCtrl', ['$scope', 'objects', function($scope, objects){
+	$scope.locked = true;
+
+	$scope.objects = objects.objects;
+
+	$scope.addObject = function(){
+		//Verifications
+		if(!$scope.title || $scope.title === ''){return;}
+
+
+		$scope.objects.push({
+			title: $scope.title,
+			description: $scope.description, 
+			priority:0,
+			specs: [
+				{name: 'Height', value: 'Very Tall', isImg: false},
+				{name: 'Color', value: 'Black', isImg: false},
+				{name: 'Image', value: 'http://www.bodyrock.tv/wp-content/uploads/2013/06/Funny-Picture-Spirit-of-Baby.jpg', isImg: true}
+			]
+		});
+		$scope.title = '';
+		$scope.description = '';
+	};
+
+	$scope.incrementPriority = function(obj){
+		obj.priority -= 1;
+	};
+
+	$scope.decreasePriority = function(obj){
+		obj.priority += 1;
+	};
+
+	$scope.lockInterface = function(){
+		$scope.locked = !$scope.locked;
+	};
+}]);
+
+app.controller('ObjectsCtrl',[
+	'$scope',
+	'$stateParams',
+	'objects',
+	function($scope,$stateParams, objects){
+		$scope.obj = objects.objects[$stateParams.id];
+	}]);
+
+app.factory('objects', [function(){
+	var o = {
+		objects: [
+
+		{
+			title: 'Chair',
+			priority: 4
+		},
+		{
+			title: 'FauxLamp',
+			priority: 3
+		},
+		{
+			title: 'LedBox',
+			priority: 2
+		},
+		{
+			title: 'Magestic',
+			priority: 1
+		}
+	
+	]
+		
+	};
+	return o;
+}]);
